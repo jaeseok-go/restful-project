a1370c5511b3d5c76a2fcdf3ce2192e65778b75f
고재석 <gojaeseok@gojaeseog-ui-MacBookAir.local>
2021-06-16 15:41:46 +0900

    feat: 프로젝트 초기 설정
    Maven 프로젝트 생성
    Dependency 추가 (Spring Web, Spring Data JPA, Database H2, Lombok)
    Port 설정 - 8081
    예제 컨트롤러 생성

---
+++ src/main/java/com/jaeseok/restfulproject/RestfulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jaeseok.restfulproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulProjectApplication.class, args);
+    }
+
+}
