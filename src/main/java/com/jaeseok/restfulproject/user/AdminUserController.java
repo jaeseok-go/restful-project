df6be82282c952deccb52381138494554174c74c
고재석 <gojaeseok@gojaeseog-ui-MacBookAir.local>
2021-06-23 00:02:42 +0900

    feat: Versioning을 위한 4가지 방법 구현
    URL에 명시하는 방법
    파라미터로 구분하는 방법
    media type으로 구분하는 방법
    헤더에 명시하는 방법

--- src/main/java/com/jaeseok/restfulproject/user/AdminUserController.java
+++ src/main/java/com/jaeseok/restfulproject/user/AdminUserController.java
@@ -3,14 +3,10 @@ package com.jaeseok.restfulproject.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,8 +29,11 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable long id) {
+    @GetMapping("/v1/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+//    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
+    public MappingJacksonValue retrieveUserV1(@PathVariable long id) {
         User user = userService.findOne(id);
 
         if (user == null) throw new UserNotFoundException(String.format("ID[%s] not found", id));
@@ -43,6 +42,28 @@ public class AdminUserController {
                 .addFilter("UserInfo", SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn"));
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filterProvider);
+
+        return mapping;
+    }
+
+    @GetMapping("/v2/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+//    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
+    public MappingJacksonValue retrieveUserV2(@PathVariable long id) {
+        User user = userService.findOne(id);
+
+        if (user == null) throw new UserNotFoundException(String.format("ID[%s] not found", id));
+
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        FilterProvider filterProvider = new SimpleFilterProvider()
+                .addFilter("UserInfoV2", SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "grade"));
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filterProvider);
 
         return mapping;
