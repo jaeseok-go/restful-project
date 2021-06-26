00656ab222b0e72cab8ba5db4a093cda4df1bdfb
jaeseok-go <gojeasuk2@naver.com>
2021-06-27 01:02:40 +0900

    feat: retrieveUser 메소드 생성
    findById로 원하는 데이터 조회
    Optional 타입 null 값 처리
    hateoas 적용하여 데이터 반환

--- src/main/java/com/jaeseok/restfulproject/user/UserJpaController.java
+++ src/main/java/com/jaeseok/restfulproject/user/UserJpaController.java
@@ -1,11 +1,15 @@
 package com.jaeseok.restfulproject.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -17,5 +21,18 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable(name = "id") long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) throw new UserNotFoundException(String.format("ID[%s] not found", id));
+
+        EntityModel<User> entityModel = EntityModel.of(user.get());
+        WebMvcLinkBuilder builder = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(builder.withRel("all-users"));
+
+        return entityModel;
     }
 }
