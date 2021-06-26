9910995e63100186c477ced2dc1839754df7bea0
jaeseok-go <gojeasuk2@naver.com>
2021-06-27 00:35:36 +0900

    feat: Entity 설정 및 H2 데이터베이스 연동
    application.yml로 H2 Database 연동
    데이터베이스 초기화 sql 파일 생성
    User 도메인 클래스 엔티티 설정

--- src/main/java/com/jaeseok/restfulproject/user/User.java
+++ src/main/java/com/jaeseok/restfulproject/user/User.java
@@ -1,29 +1,30 @@
 package com.jaeseok.restfulproject.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-// @JsonFilter("UserInfo")
-
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
 
+    @Id @GeneratedValue
     private Long id;
 
-    @Min(value = 2, message = "name은 두 글자 이상 입력해 주세요.")
+    @Size(min = 2, message = "name은 두 글자 이상 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into USER values (1, sysdate(), 'User1', 'test1', '931212-1234567');
+insert into USER values (2, sysdate(), 'User2', 'test2', '941212-1234567');
+insert into USER values (3, sysdate(), 'User3', 'test3', '951212-1234567');
